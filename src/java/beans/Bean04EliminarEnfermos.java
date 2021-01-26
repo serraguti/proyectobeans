65cc9a33845aa5939c7abe08765d93a5fd1d491b
Serra <pacoserranox@gmail.com>
2021-01-26 16:54:56 +0100

    Merge origin/master

--- src/java/beans/Bean04EliminarEnfermos.java
+++ src/java/beans/Bean04EliminarEnfermos.java
@@ -5,10 +5,50 @@ Si pulsamos sobre un botón eliminar, borramos al enfermo.
  */
 package beans;
 
-/**
- *
- * @author Serra
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import oracle.jdbc.OracleDriver;
+
 public class Bean04EliminarEnfermos {
 
+    //QUE FUNCIONALIDAD DE LA BBDD NECESITAMOS??
+    //DIBUJAR ENFERMOS (String)
+    //ELIMINAR ENFERMOS  (void)
+    private Connection getConnection() throws SQLException {
+        DriverManager.registerDriver(new OracleDriver());
+        String cadena = "jdbc:oracle:thin:@LOCALHOST:1521:XE";
+        Connection cn
+                = DriverManager.getConnection(cadena, "system", "oracle");
+        return cn;
+    }
+
+    public String getListaEnfermos() throws SQLException {
+        Connection cn = this.getConnection();
+        String sql = "select * from enfermo";
+        Statement st = cn.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        String html = "<ul>";
+        while (rs.next()) {
+            String apellido = rs.getString("APELLIDO");
+            String inscripcion = rs.getString("INSCRIPCION");
+            html += "<li>" + apellido + ", " + inscripcion + "</li>";
+        }
+        html += "</ul>";
+        rs.close();
+        cn.close();
+        return html;
+    }
+
+    public void eliminarEnfermo(int inscripcion) throws SQLException {
+        Connection cn = this.getConnection();
+        String sql = "delete from enfermo where inscripcion=?";
+        PreparedStatement pst = cn.prepareStatement(sql);
+        pst.setInt(1, inscripcion);
+        pst.executeUpdate();
+        cn.close();
+    }
 }
--- web/web04eliminarenfermos.jsp
+++ web/web04eliminarenfermos.jsp
@@ -3,8 +3,10 @@ Dibujar todos los enfermos al iniciar la página.
 Una caja para escribir la inscripción.
 Si pulsamos sobre un botón eliminar, borramos al enfermo.
 --%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<jsp:useBean id="beanenfermos"
+             class="beans.Bean04EliminarEnfermos"
+             scope="request"/>
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,6 +14,20 @@ Si pulsamos sobre un botón eliminar, borramos al enfermo.
         <title>JSP Page</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <h1>Eliminar enfermos Bean</h1>
+        <form method="post">
+            <label>Inscripción: </label>
+            <input type="text" name="cajainscripcion" required/>
+            <button type="submit">Eliminar enfermo</button>
+        </form>
+        <%
+        String dato = request.getParameter("cajainscripcion");
+        if (dato != null){
+            int inscripcion = Integer.parseInt(dato);
+            beanenfermos.eliminarEnfermo(inscripcion);
+        }
+        %>
+        
+        <%=beanenfermos.getListaEnfermos()%>
     </body>
 </html>
